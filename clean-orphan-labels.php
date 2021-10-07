a152b82edc2289aca44fbf0408a32c900789afd5
George Constantinou <georgec20001@gmail.com>
2021-10-07 11:26:49 +0300

    Initial commit

---
+++ clean-orphan-labels.php
@@ -0,0 +1,179 @@
+<?php
+
+declare(strict_types=1);
+
+final class OrphanLabelsCleaner
+{
+    private string $baseUrl;
+    private string $authToken;
+
+    private function __construct(string $baseUrl, string $authToken)
+    {
+        $this->baseUrl = $baseUrl;
+        $this->authToken = $authToken;
+    }
+
+    public static function fromCli(array $arguments): self
+    {
+        $errors = [];
+
+        if (! array_key_exists(1, $arguments)) {
+            $errors[] = 'Please provide Gitlab instance base URL.';
+        }
+
+        if (! array_key_exists(2, $arguments)) {
+            $errors[] = 'Please provide Gitlab private token.';
+        }
+
+        if ([] !== $errors) {
+            self::exitWithError($errors);
+        }
+
+        return new self($arguments[1], $arguments[2]);
+    }
+
+    public function clean(): void
+    {
+        foreach ($this->getProjects() as $project) {
+            $projectId = $project['id'] ?? -1;
+
+            echo 'Processing project "' . $project['name'] . ' [' . $projectId . ']" ..' . PHP_EOL;
+
+            $labels = $this->getProjectLabels($projectId);
+            $orphanLabels = $this->filterLabels($labels);
+
+            $this->deleteLabels($projectId, $orphanLabels);
+
+            echo PHP_EOL;
+        }
+
+        echo PHP_EOL;
+    }
+
+    private function getProjects(): array
+    {
+        $resource = curl_init();
+
+        curl_setopt_array($resource, [
+            CURLOPT_URL => sprintf('%s/api/v4/projects?simple=true&per_page=100', $this->baseUrl),
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_HTTPHEADER => [sprintf('PRIVATE-TOKEN: %s', $this->authToken)]
+        ]);
+
+        $response = curl_exec($resource);
+
+        curl_close($resource);
+
+        $response = (array)json_decode($response, true);
+
+        if (array_key_exists('error', $response)) {
+            self::exitWithError([$response['error_description'] ?? '']);
+        }
+
+        return $response;
+    }
+
+    private static function exitWithError(array $errors) {
+        foreach ($errors as $error) {
+            $error = '' === trim($error) ? 'Unknown.' : $error;
+
+            echo sprintf('[ERROR] %s ', $error) . PHP_EOL;
+        }
+
+        echo PHP_EOL;
+        exit(1);
+    }
+
+    private function getProjectLabels(int $projectId): array
+    {
+        $resource = curl_init();
+
+        curl_setopt_array($resource, [
+            CURLOPT_URL => sprintf(
+                '%s/api/v4/projects/%d/labels?with_counts=true&per_page=100',
+                $this->baseUrl,
+                $projectId
+            ),
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_HTTPHEADER => [sprintf('PRIVATE-TOKEN: %s', $this->authToken)]
+        ]);
+
+        $response = curl_exec($resource);
+
+        curl_close($resource);
+
+        $response = (array)json_decode($response, true);
+
+        if (array_key_exists('error', $response)) {
+            self::exitWithError([$response['error_description'] ?? '']);
+        }
+
+        return $response;
+    }
+
+    private function filterLabels(array $labels): array
+    {
+        return array_filter($labels, function ($label) {
+            if (! $label['is_project_label']) {
+                return false;
+            }
+
+            if (0 < $label['open_issues_count']) {
+                echo '[INFO] ' . $label['name'] . ' has open issues' . PHP_EOL;
+                return false;
+            }
+
+            if (0 < $label['closed_issues_count']) {
+                echo '[INFO] ' . $label['name'] . ' has closed issues' . PHP_EOL;
+                return false;
+            }
+
+            if (0 < $label['open_merge_requests_count']) {
+                echo '[INFO] ' . $label['name'] . ' has open merge requests' . PHP_EOL;
+                return false;
+            }
+
+            return true;
+        });
+    }
+
+    private function deleteLabels(int $projectId, array $labels): void
+    {
+        foreach ($labels as $label) {
+            $labelId = $label['id'] ?? -1;
+
+            echo 'deleting label "' . $label['name'] . ' [' . $labelId . ']" ..';
+            $this->deleteLabel($projectId, $labelId);
+            echo ' done' . PHP_EOL;
+        }
+    }
+
+    private function deleteLabel(int $projectId, int $labelId): void
+    {
+        $resource = curl_init();
+
+        curl_setopt_array($resource, [
+            CURLOPT_URL => sprintf(
+                '%s/api/v4/projects/%d/labels/%d',
+                $this->baseUrl,
+                $projectId,
+                $labelId
+            ),
+            CURLOPT_CUSTOMREQUEST => 'DELETE',
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_HTTPHEADER => [sprintf('PRIVATE-TOKEN: %s', $this->authToken)]
+        ]);
+
+        $response = curl_exec($resource);
+
+        curl_close($resource);
+
+        $response = (array)json_decode($response, true);
+
+        if (array_key_exists('error', $response)) {
+            self::exitWithError([$response['error_description'] ?? '']);
+        }
+    }
+}
+
+OrphanLabelsCleaner::fromCli($argv)->clean();
